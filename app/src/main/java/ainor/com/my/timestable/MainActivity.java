6830258b6b9a3759cd7a1789b33e260e0ca134f2
Ainor Syahrizal <macintorch@gmail.com>
2016-12-18 07:27:00 +0800

    add list of initial sifir

--- app/src/main/java/ainor/com/my/timestable/MainActivity.java
+++ app/src/main/java/ainor/com/my/timestable/MainActivity.java
@@ -2,6 +2,10 @@ package ainor.com.my.timestable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //List of sifir
+
+        ListView sifirListView = (ListView) findViewById(R.id.sifirListView);
+
+        ArrayList sifir = new ArrayList();
+
+        sifir.add(1);
+        sifir.add(2);
+        sifir.add(3);
+        sifir.add(4);
+        sifir.add(5);
+        sifir.add(6);
+        sifir.add(7);
+        sifir.add(8);
+        sifir.add(9);
+        sifir.add(10);
+        sifir.add(11);
+        sifir.add(12);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, sifir);
+
+        sifirListView.setAdapter(arrayAdapter);
+
+
+
     }
 }
